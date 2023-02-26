bebcb6c11b0db90fc84385e96966dbde657c8bc1
cms <audtlr93@naver.com>
2023-02-26 12:16:24 +0900

    style: 컴포넌트 이름 변경에 따른 import 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Greeting from "./whole-sections/section0-greeting/greeting";
 import Introduce from "./whole-sections/section1-introduction/introduce";
 import MyBackgorund from "./whole-sections/section2-mybackground/my-background";
-import MySkill from "./whole-sections/section3-myskill/my-skill";
+import MySkill from "./whole-sections/section3-myskill/my-skills";
 import Studies from "./whole-sections/section4-study/study";
 import Projects from "./whole-sections/section5-project/projects";
 import { Transition } from "react-transition-group";
